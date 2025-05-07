08191faa2f3feb6003acfd301cb33460101d5dd6
Johan Schmidt <johan.schmidt@epfl.ch>
2025-05-07 16:55:04 +0200

    Created script to build and export a pKa dataset from a list of molecule names

---
+++ src/ppchem-project/init_data.py
@@ -0,0 +1,7 @@
+from core import build_data_table
+
+# Liste de molécules pour remplir la base
+molecule_names = ["acetic acid", "ethanol", "ammonia", "formic acid", "benzene"]
+
+# Génération du fichier data.csv
+build_data_table(molecule_names)
